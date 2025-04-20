e0601feb83738e6557e82863d3b419f4a6deb8fb
sunboy888 <141954158+sunboy888@users.noreply.github.com>
2025-04-21 02:18:34 +0800

    更新 sn.js

--- sn.js
+++ sn.js
@@ -1,9 +1,15 @@
+/******************************
+脚本功能：关不掉的闹钟-解锁会员
+脚本作者：afengye
+脚本频道：https://t.me/afengye
+更新时间：2024-08-03
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
 [rewrite_local]
 ^https:\/\/user-snow-api.snow.me\/v1\/purchase\/subscription\/subscriber\/status url script-response-body https://raw.githubusercontent.com/sunboy888/new/main/sn.js
 [mitm] 
-hostname = user-snow-api.snow.me
+hostname = nao.beijingmorning.cn
 *******************************/
-
 var chxm1023 = JSON.parse($response.body);
 
 chxm1023 = {
