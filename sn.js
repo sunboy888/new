65ef4e6c33dca5cd95f6a3828e79552a69662ab7
sunboy888 <141954158+sunboy888@users.noreply.github.com>
2025-04-20 23:24:25 +0800

    Update and rename danbo.js to sn.js

---
+++ sn.js
@@ -0,0 +1,30 @@
+[rewrite_local]
+^https:\/\/user-snow-api.snow.me\/v1\/purchase\/subscription\/subscriber\/status url script-response-body https://raw.githubusercontent.com/afengye/QX/main/danbo.js
+[mitm] 
+hostname = user-snow-api.snow.me
+*******************************/
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {
+  "result" : {
+    "tickets" : [
+
+    ],
+    "vipType" : "VIP_SUBSCRIBER",
+    "activated" : true,
+    "products" : [
+      {
+        "status" : "ACTIVE",
+        "originalTransactionId" : "90002415884112",
+        "productId" : "com.campmobile.snow.subscribe.onemonth",
+        "startDate" : 1744990894000,
+        "storeType" : "APPLE",
+        "expireDate" : 253392455349000,
+        "managed" : false
+      }
+    ]
+  }
+};
+
+$done({body : JSON.stringify(chxm1023)});
