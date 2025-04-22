1d4c8a351d5b18f4c5d5bbdbde351b8cf3d07ce2
sunboy888 <141954158+sunboy888@users.noreply.github.com>
2025-04-22 14:38:01 +0800

    Update op.js

--- op.js
+++ op.js
@@ -3,5 +3,28 @@ if (typeof $response === "undefined") {
   delete $request.headers["X-RevenueCat-ETag"];
   $done({ headers: $request.headers });
 } else {
-  let _0x1f23=['subscribers','com.withopal.opal.premiumtier2yearlyalt','premium','entitlements','subscribers','subscriptions','app_store','PURCHASED','2099-09-09T09:09:09Z','2023-02-23T02:33:33Z'];let body=JSON.parse($response.body);body[_0x1f23[0]]=body[_0x1f23[0]]||{};body[_0x1f23[0]][_0x1f23[5]]=body[_0x1f23[0]][_0x1f23[5]]||{};body[_0x1f23[0]][_0x1f23[3]]=body[_0x1f23[0]][_0x1f23[3]]||{};let fakeData={"expires_date":_0x1f23[8],"original_purchase_date":_0x1f23[9],"purchase_date":_0x1f23[9],"ownership_type":_0x1f23[7],"store":_0x1f23[6]};body[_0x1f23[0]][_0x1f23[5]][_0x1f23[1]]=fakeData;body[_0x1f23[0]][_0x1f23[3]][_0x1f23[2]]={...fakeData,"product_identifier":_0x1f23[1]};$done({body:JSON.stringify(body)});
+  let body = JSON.parse($response.body);
+
+  const entitlementName = "premium";
+  const productId ="com.withopal.opal.premiumtier2yearlyalt";
+
+  const fakeData = {
+    "expires_date": "9999-09-09T09:09:09Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+
+  body.subscriber = body.subscriber || {};
+  body.subscriber.subscriptions = body.subscriber.subscriptions || {};
+  body.subscriber.entitlements = body.subscriber.entitlements || {};
+
+  body.subscriber.subscriptions[productId] = fakeData;
+  body.subscriber.entitlements[entitlementName] = {
+    ...fakeData,
+    product_identifier: productId
+  };
+
+  $done({ body: JSON.stringify(body) });
 }
